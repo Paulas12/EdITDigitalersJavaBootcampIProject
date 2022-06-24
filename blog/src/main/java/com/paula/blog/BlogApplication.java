a69db1687a4607a6dbe4b44e9224ea95d3662c08
Paula <Paula@DESKTOP-V077L0V>
2022-06-24 15:20:24 -0300

    Implementé clase usuario

--- blog/src/main/java/com/paula/blog/BlogApplication.java
+++ blog/src/main/java/com/paula/blog/BlogApplication.java
@@ -5,6 +5,53 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogApplication {
+	public BlogApplication(Spring userName, Spring name, Spring lastName) {
+		super();
+		this.userName = userName;
+		this.name = name;
+		this.lastName = lastName;
+	}
+
+
+
+
+
+	private Spring userName="";
+	private Spring name="";
+	private Spring lastName="";
+	
+	public Spring getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(Spring userName) {
+		this.userName = userName;
+	}
+
+
+	public Spring getName() {
+		return name;
+	}
+
+
+	public void setName(Spring name) {
+		this.name = name;
+	}
+
+
+	public Spring getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(Spring lastName) {
+		this.lastName = lastName;
+	}
+
+
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
